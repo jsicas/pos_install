8f3205191208a0c959351ea8c7f9db5d2ad5d9af
jsicas <jvsiqueira2013@gmail.com>
2025-10-04 01:24:52 -0300

    install ubuntu

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scripts de Pós Instalação
+
+<p align='justify'>
+O objetivo desse script é facilitar a pós-instalação, focando no Ubunut 24.04, baixando aplicativos e aplicando configurações ([dotfiles](https://github.com/jsicas/dotfiles)).
+</p>
--- mk_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# criando links simbólicos
-#ln -sf ~/.dotfiles/.bashrc ~/.bashrc              # executa sempre que o terminal abre
-#ln -sf ~/.dotfiles/.profile ~/.profile            # executado no login quando .bash_profile não existir
-ln -sf ~/.dotfiles/.XCompose ~/.XCompose          # atalhos de teclado personalizados
-#ln -sf ~/.dotfiles/.bash_profile ~/.bash_profile  # executando no login
-ln -sf ~/.dotfiles/.vimrc ~/.vimrc                # shortcurts personalizados
