c88d7df9624c78a5f4baf4cd3d8c547c37247d1a
jsicas <jvsiqueira2013@gmail.com>
2025-10-18 19:15:22 -0300

    Update fedora

--- fedora_install.sh
+++ fedora_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash 
 
 # script para automatizar a pós instalação do Fedora Workstation 42 com Gnome.
 
@@ -17,8 +17,8 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 
 
-# instalações ---------------------------------------------
-# dnf e flatpak =====================
+# instalações =============================================
+# dnf e flatpak ---------------------
 dnf_install=(
 	# RPM Fusion Freen e Nonfree
 	https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm        # Free
@@ -47,21 +47,42 @@ sudo dnf install -yq ${dnf_install[@]}
 flatpak install -y flathub ${flat_install[@]}
 
 
-# instalações manuais ================
+# instalações manuais ---------------
 # Rstudio (Fedora 41)
 echo 'Instalando Rstudio...'
 wget -O rstudio.rpm https://download1.rstudio.org/electron/rhel9/x86_64/rstudio-2025.09.1-401-x86_64.rpm
 sudo dnf install -y ./rstudio.rpm
 
+# TeX Live
+echo 'Removendo TeX Live padrão...'
+sudo dnf remove -y texlive*
+echo 'Instalando TeXlive...'
+wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+zcat < install-tl-unx.tar.gz | tar xf -
+cd install-tl-2*
+sudo perl ./install-tl -profile texlive.profile
 
-# configurações do DE -------------------------------------
+
+# configurações do DE =====================================
 gsettings set org.gnome.desktop.interface clock-format '24h'
 
+# atalho <super> + t para abrir terminal
+# deve-se verificar antes quais atalhos já estão definidos, como não havia nenhum, foi utilizado custom0.
+#gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings  # ver atalhos já definidos
+slot='/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$slot name 'Abrir Terminal (Ptyxis)'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$slot command 'ptyxis'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:$slot binding '<Super>t'
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['$slot']"
 
-# dotfiles ------------------------------------------------
+
+# dotfiles ================================================
 echo 'Baixando configurações (dotfiles)...'
 git clone https://github.com/jsicas/dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/mk_config.sh
 
 # finalizar instalando as extensões
-# dash to panel, search light
+# - dash to panel: configurações da barra de tarefas;
+# - search light: laucher de apps;
+# - AppIndicator and KStatusNotifierItem Support: habilita suporte à indicadores na barra de tarefas;
+# - Disable unredirect fullscreen windows: impede que janelas maximizadas sejam consideradas maximizadas, isso resolve o problema de janelas piscando devido à incompatibilidade de aplicativos que utilizam aceleração de hardware num setup com placa de vídeo Nvidia.
