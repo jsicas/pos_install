b5327eb97741b188554035b52357a6160e7c084f
jsicas <jvsiqueira2013@gmail.com>
2025-12-11 16:16:10 -0300

    add scripts

---
+++ scripts/vicinae.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# script para instalação/atualização do vicinae, documentação disponível em https://docs.vicinae.com
+
+set -e
+
+curl -fsSL https://vicinae.com/install.sh | sudo bash
